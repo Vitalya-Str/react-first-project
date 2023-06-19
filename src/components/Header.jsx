62eb395e09e73119bc21e50863ea0a350cc79028
Vitalya-Str <Strela2789@gmail.com>
2023-06-19 23:32:33 +0600

    Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = ()=>{
+    return <header className='header'>
+        <img src="https://1000logos.net/wp-content/uploads/2019/12/Volkswagen-logo.jpg" alt="logo"/>
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+
+const Profile = ()=>{
+    return <div className='content'>
+        <div> <img className='img__content' src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+        </div>
+        <div>ava + description</div>
+        <div> My posts</div>
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+
+    </div>
+}
+
+export default Profile
