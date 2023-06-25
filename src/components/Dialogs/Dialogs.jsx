48d28ce22a19e15033e1762a7472a2745c8e2042
Vitalya-Str <Strela2789@gmail.com>
2023-06-25 11:58:27 +0600

    video23(Route)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,31 @@
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
-        </div>)
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + '' + s.active}>
+                    <NavLink to='dialogs/1'> Kama </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='dialogs/2'>Julia</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='dialogs/3'>Artem</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='dialogs/4'>Vitalya</NavLink>
+                </div>
+            </div>
+            <div className={s.massages}>
+                <div className={s.massage}>Hello Wold!</div>
+                <div className={s.massage}>How old are you?</div>
+                <div className={s.massage}>What is your name?</div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,16 +2,18 @@ import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
-    return (<div>
+    return (
         <div>
-            <img src='https://s3.amazonaws.com/images.seroundtable.com/google-css-images-1515761601.jpg'/>
+            <div>
+                <img src='https://s3.amazonaws.com/images.seroundtable.com/google-css-images-1515761601.jpg'/>
+            </div>
+
+            <div className={s.item}>ava + description</div>
+
+            <MyPosts/>
+
         </div>
-
-        <div className={s.item}>ava + description</div>
-
-        <MyPosts/>
-
-    </div>)
+    )
 }
 
 export default Profile
