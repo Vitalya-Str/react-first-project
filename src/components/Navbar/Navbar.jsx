22092832c7c6371c9f436716d33e0e66d2586e32
Vitalya-Str <Strela2789@gmail.com>
2023-07-24 23:19:11 +0600

    video53-2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,6 +17,9 @@ const Navbar = (props) => {
                <NavLink to='/dialogs' className={setActive}>Massages</NavLink>
             </div>
             <div className={s.item}>
+               <NavLink to='/users' className={setActive}>Users</NavLink>
+            </div>
+            <div className={s.item}>
                <NavLink to='/news' className={setActive}>News</NavLink>
             </div>
             <div className={s.item}>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,14 +5,11 @@ import React from "react";
 
 
 class Users extends React.Component {
-   constructor(props) {
-      super(props);
+   componentDidMount() {
       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-         props.setUsers(response.data.items)
+         this.props.setUsers(response.data.items)
       })
-
    }
-
    render() {
       return (<div>
          {
