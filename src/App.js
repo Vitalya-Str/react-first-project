ea0d060def617e55e1dc8069d6f2cb2037e0feac
Vitalya-Str <Strela2789@gmail.com>
2023-08-01 22:55:01 +0600

    video62

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import UsersContainer from  "./components/Users/UsersContainer"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
-const App = (props) => {
+const App = () => {
 
    return (
       <BrowserRouter>
          <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className='app-wrapper-content'>
                <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = ()=>{
-    return <header className={s.header}>
-        <img  src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="logo"/>
-    </header>
+const Header = (props) => {
+   return <header className={s.header}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="logo"/>
+
+      <div className={s.headerLogin}>
+         {props.isAuth ? props.login : <NavLink to={'login/'}>Login</NavLink>}
+      </div>
+
+   </header>
 }
 
 export default Header
